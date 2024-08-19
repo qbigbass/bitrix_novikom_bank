71b39d91a5d955377501befbf3e4df4c0382c4f5
Вотинцев Андрей Александрович <votincev-aa@galagodigital.ru>
2024-08-19 16:13:20 +0500

    GLGNVKB-3 выносит форму заказа звонка в подключаемый файл

--- s1/for-private-clients/credits/index.php
+++ s1/for-private-clients/credits/index.php
@@ -1230,50 +1230,8 @@ Asset::getInstance()->addJsAndCss('loans');
         </div>
 
     </section>
-    <section class="section-layout section-call-me-back section-layout--bg-undefined">
 
-        <div class="content-container">
-            <div class="section-call-me-back__container">
-                <div class="section-call-me-back__content">
-                    <div class="section-title section-call-me-back__title">
-                        <h3 class="section-title__text headline-2">
-                            Остались вопросы
-                        </h3>
-
-                    </div>
-                    <div class="body-l-light">Оставьте свой телефон и мы перезвоним вам, либо задайте вопрос в чате</div>
-                </div>
-                <div class="section-call-me-back__help">
-                    <div class="section-call-me-back__links">
-                        <button class="a-button a-button--m a-button--white">Открыть чат
-                        </button>
-                        <!--<AButton element="a" href="#" size="m" color="white">Перейти в раздел Помощь</AButton>-->
-                    </div>
-                    <div class="section-call-me-back__confirm dark-70 body-s-light">Нажимая кнопку «Перезвоните мне», вы&nbsp;соглашаетесь с&nbsp;условиями предоставления информации</div>
-                </div>
-                <div class="section-call-me-back__form">
-                    <div class="form-call-me-back form-call-me-back--white">
-
-                        <form class="form-call-me-back__form" action="/" method="POST">
-                            <div class="a-input js-a-input a-input--ms">
-                                <label for="mobile-phone" class="a-input__label body-s-heavy">Мобильный телефон</label>
-                                <input id="mobile-phone" class="a-input__input" placeholder="+7" aria-describedby="mobile-phone-hint">
-
-                            </div>
-                            <button type="button" class="a-button a-button--lm a-button--primary a-button--full">Перезвоните мне
-                            </button>
-                        </form>
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="section-call-me-back__background">
-            <img src="/frontend/build/assets/section-call-me-back-bg.svg">
-        </div>
-
-    </section>
+    <?$APPLICATION->IncludeFile('/local/php_interface/include/request_call.php');?>
 
 </main>
 <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
