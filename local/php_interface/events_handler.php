95fd10a3a29c7b462478b02183bd2756304d8523
Aleksey <badproper98@gmail.com>
2024-08-23 10:14:39 +0300

    GLGNVKB-9 | Создает подписку на событие

--- local/php_interface/events_handler.php
+++ local/php_interface/events_handler.php
@@ -3,3 +3,5 @@ use Bitrix\Main;
 $eventManager = Main\EventManager::getInstance();
 
 //$eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', ['Galago\UserTypesField\PropSectionCustom', 'GetUserTypeDescription']);
+
+$eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', ['Galago\CustomFields\CustomElementProperty', 'GetUserTypeDescription']);
