3b3f4db43248ce62d66a345889cb4e3347e3c6c9
Ilya Ponomarev <admin@iMac-admin.local>
2024-08-27 12:35:21 +0500

    GLGNVKB-6 вернет обратно конфиг Astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/frontend/build',
+	// base: '/frontend/build',
 	outDir: './build',
 	devToolbar: {
 		enabled: false
