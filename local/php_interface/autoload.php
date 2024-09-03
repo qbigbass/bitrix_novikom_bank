e870a7c8aab7eb1e947404129b8d9d974b7fbfe6
Aleksey <badproper98@gmail.com>
2024-09-03 11:26:44 +0300

    GLGNVKB-9 | Убирает лишний код, подключает новый класс

--- local/php_interface/autoload.php
+++ local/php_interface/autoload.php
@@ -4,4 +4,5 @@ use Bitrix\Main\Loader;
 Loader::registerAutoLoadClasses(null, [
     'Galago\Frontend\Asset' => LIBRARY_DIRECTORY . 'Frontend/Asset.php',
     'Galago\CustomFields\CustomElementProperty' => LIBRARY_DIRECTORY . 'CustomFields/CustomElementProperty.php',
+    'Galago\Classes\CustomIblockElements' => LIBRARY_DIRECTORY . 'Classes/CustomIblockElements.php',
 ]);
--- local/templates/main/components/bitrix/news/cards/bitrix/news.list/.default/result_modifier.php
+++ local/templates/main/components/bitrix/news/cards/bitrix/news.list/.default/result_modifier.php
@@ -1,21 +1,17 @@
 <?php
 
-
-use Bitrix\Iblock\Model\Section;
-
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
 {
 	die();
 }
 
+use Bitrix\Iblock\Model\Section;
+
 /** @var array $arParams */
 /** @var array $arResult */
 
 $elementEntity = \Bitrix\Iblock\Iblock::wakeUp(iblock('inner_card_info'))->getEntityDataClass();
-$arrItemIds = [];
-$arrItems = [];
 $arrSectionIds = [];
-$result = [];
 
 // получение польз.свойства раздела
 foreach ($arResult['ITEMS'] as $item) {
@@ -28,45 +24,14 @@ $sectionQuery = Section::compileEntityByIblock($arParams['IBLOCK_ID'])::query()
     ->exec()->fetchAll();
 
 foreach ($arResult['ITEMS'] as $key => &$item) {
+    $convertFromSprintEditorBlock = json_decode($item['PROPERTIES']['DETAIL_CARD']['~VALUE'], true)['blocks'][0];
+
+    if (isset($convertFromSprintEditorBlock['info_elements'])) {
+        $item['anchoredElements'] = $convertFromSprintEditorBlock['info_elements'];
+    }
+
     $findKey = array_search($item['IBLOCK_SECTION_ID'], array_column($sectionQuery,'ID'));
     if ($findKey !== false) {
         $item['IBLOCK_SECTION_PROP_TYPE_CARDS'] = $sectionQuery[$findKey]['UF_TYPE_CARDS'];
-    }
-    $arrItemIds[$item['ID']] = $item['PROPERTIES']['DETAIL_INFO_CARD']['VALUE'];
-}
-
-array_walk_recursive($arrItemIds, function ($item, $key) use (&$result) {
-    $result[] = $item;
-});
-
-$innerCardInfoElements = $elementEntity::query()
-    ->setSelect(['ID','NAME', 'CODE'])
-    ->setFilter(['ID' => $result])
-    ->setCacheTtl(7200)
-    ->exec()->fetchAll();
-
-// ищет и сохраняет информацию по элементам
-foreach ($arrItemIds as $parentElementIdKey => $arrChildElementId) {
-
-    if ($arrChildElementId) {
-        $arrItems[$parentElementIdKey] = array_map(function ($item) use ($innerCardInfoElements) {
-            $findKey = array_search($item, array_column($innerCardInfoElements, 'ID'));
-
-            if ($findKey !== false) {
-                return $innerCardInfoElements[$findKey];
-            } else {
-                return [];
-            }
-
-        }, $arrChildElementId);
-    }
-
-}
-
-if ($arrItems) {
-    foreach ($arResult['ITEMS'] as $key => $items) {
-        if ($arrItems[$items['ID']]) {
-            $arResult['ITEMS'][$key]['anchoredElements'] = $arrItems[$items['ID']];
-        }
     }
 }
