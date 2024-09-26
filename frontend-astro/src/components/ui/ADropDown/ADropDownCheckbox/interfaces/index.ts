cb5afd8d1277bd2b0ef49d7562c6451e83868b23
Ilya Ponomarev <admin@iMac-admin.local>
2024-09-26 15:11:46 +0500

    GLGNVKB-6 | восстановит работу версии Astro

---
+++ frontend-astro/src/components/ui/ADropDown/ADropDownCheckbox/interfaces/index.ts
@@ -0,0 +1,24 @@
+import type { ACheckboxCustomEvent } from "@components/ui/ACheckbox/interfaces";
+import type { ACheckbox } from "@components/ui/ACheckbox/interfaces";
+
+export interface ADropDownCheckboxEvent extends ACheckboxCustomEvent {}
+
+export interface ADropDownCheckboxElements {
+    root: HTMLDivElement;
+}
+
+export interface ADropDownCheckboxComponents {
+    checkbox: ACheckbox;
+}
+
+export interface ADropDownCheckboxState {
+    elements: ADropDownCheckboxElements;
+    components: ADropDownCheckboxComponents;
+    checked: boolean;
+    name: string;
+    displayValue: string;
+}
+
+export interface ADropDownCheckbox extends HTMLDivElement {
+    $state: ADropDownCheckboxState;
+}
