98b4dbf8533ea6f2426b5ab0fe537b968491cad9
Ilya Ponomarev <admin@iMac-admin.local>
2024-10-02 14:47:29 +0500

    GLGNVKB-6 | добавит планиг select2

---
+++ frontend/src/js/components/select2.js
@@ -0,0 +1,18 @@
+import $ from "jquery"
+import select2 from "select2";
+import "select2/dist/css/select2.css";
+
+const SELECT2_CLASSES = {
+    root: '.js-select2',
+}
+
+
+function initSelect2() {
+    $(SELECT2_CLASSES.root).each(function() {
+        $(this).select2({
+            minimumResultsForSearch: -1
+        });
+    });
+}
+
+export default initSelect2;
