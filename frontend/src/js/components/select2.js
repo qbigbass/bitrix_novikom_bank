0ea31ca03161d172d6f3dbb606b071d1cd7a4871
Ilya Ponomarev <admin@iMac-admin.local>
2024-10-03 14:18:16 +0500

    GLGNVKB-6 | кастомизация select всех состояний и размеров + добавлены примеры на UI страницу

--- frontend/src/js/components/select2.js
+++ frontend/src/js/components/select2.js
@@ -3,15 +3,24 @@ import select2 from "select2";
 import "select2/dist/css/select2.css";
 
 const SELECT2_CLASSES = {
-    root: '.js-select2',
+    root: '.js-select',
+    smallSize: 'form-select--size-small',
+    selectSmallClass: 'select2-selection--size-small',
+}
+
+const defaultSelectOptions = {
+    minimumResultsForSearch: -1,
+    width: '100%',
 }
 
 
 function initSelect2() {
     $(SELECT2_CLASSES.root).each(function() {
-        $(this).select2({
-            minimumResultsForSearch: -1,
-        });
+        const options = {...defaultSelectOptions};
+        if ($(this).hasClass(SELECT2_CLASSES.smallSize)) {
+            options.selectionCssClass = SELECT2_CLASSES.selectSmallClass
+        }
+        $(this).select2(options);
     });
 }
 
