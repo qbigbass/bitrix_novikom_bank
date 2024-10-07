80990b466031c92cbc3993f0c543c56202c1c561
persol <persol2013@gmail.com>
2024-10-07 16:53:27 +0500

    GLGNVKB-11 | исправит инициализацию полигона, если изначально вкладка была закрыта

--- frontend/src/js/components/tabs.js
+++ frontend/src/js/components/tabs.js
@@ -1,14 +1,35 @@
 export function initTabsContent() {
+    const updatePolygonInTabContent = (el) => {
+        const polygonArray = el.querySelectorAll('.js-polygon-container-polygon');
+
+        polygonArray.forEach((polygon) => polygon.dispatchEvent(new Event("resize")));
+    };
+
     const tabsCollapseArray = document.querySelectorAll('.tabs-with-content .collapse');
 
     tabsCollapseArray.forEach((el) => {
+        let tabContentIsVisible = () => el.clientHeight !== 0;
         const linkEl = document.querySelector(`.tabs-panel__list-item-link[data-bs-target="#${el.id}"`);
 
+        // Fixes initialization of active tab
         if (linkEl.classList.contains('active')) {
             el.classList.add('show');
         }
 
-        el.addEventListener('show.bs.collapse', event => linkEl.classList.add('active'))
-        el.addEventListener('hide.bs.collapse', event => linkEl.classList.remove('active'))
+        // Initializes a polygon when tab content is displayed
+        if (!tabContentIsVisible()) {
+            const resizeObserver = new ResizeObserver(entries => {
+                if (tabContentIsVisible()) {
+                    updatePolygonInTabContent(el);
+                    resizeObserver.disconnect();
+                }
+            });
+            
+            // start observing a DOM node
+            resizeObserver.observe(el);
+        }
+
+        el.addEventListener('show.bs.collapse', (event) => linkEl.classList.add('active'))
+        el.addEventListener('hide.bs.collapse', (event) => linkEl.classList.remove('active'))
     });
 }
