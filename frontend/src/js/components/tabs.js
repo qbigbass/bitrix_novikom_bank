de5611af7714a3a9f7c31d28528e0c16c0c636ff
persol <persol2013@gmail.com>
2024-10-07 15:43:43 +0500

    GLGNVKB-11 | исправит инициализацию табов с контентом

--- frontend/src/js/components/tabs.js
+++ frontend/src/js/components/tabs.js
@@ -1,8 +1,12 @@
-export function initTabsContentEvents() {
+export function initTabsContent() {
     const tabsCollapseArray = document.querySelectorAll('.tabs-with-content .collapse');
 
     tabsCollapseArray.forEach((el) => {
         const linkEl = document.querySelector(`.tabs-panel__list-item-link[data-bs-target="#${el.id}"`);
+
+        if (linkEl.classList.contains('active')) {
+            el.classList.add('show');
+        }
 
         el.addEventListener('show.bs.collapse', event => linkEl.classList.add('active'))
         el.addEventListener('hide.bs.collapse', event => linkEl.classList.remove('active'))
