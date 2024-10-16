3f6a190638186c668581b52f12aad00ad3b3d6b6
vimbatu <vimbatu@yandex.ru>
2024-10-16 19:25:33 +0300

    GLGNVKB-12 | добавляет кастомное свойство инфоблока для вывода строки с тремя полями

---
+++ local/php_interface/autoload.php
@@ -0,0 +1,6 @@
+<?php
+use Bitrix\Main\Loader;
+
+Loader::registerAutoLoadClasses(null, [
+    'Lib\UserType\CUserTypeStringDescr' => '/local/php_interface/Lib/UserType/CUserTypeStringDescr.php',
+]);
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,8 @@
 <?php
 require 'constants.php';
 require 'functions.php';
+
+require dirname(__FILE__) . '/autoload.php';
+require dirname(__FILE__) . '/event_handler.php';
+
+
