80faebaee0dd1f4c59088dfdd9976eeb9dfb740c
vimbatu <vimbatu@yandex.ru>
2024-10-16 19:42:17 +0300

    GLGNVKB-12 | добавляет компонент хлебных крошек

---
+++ local/templates/main/components/bitrix/breadcrumb/.default/template.php
@@ -0,0 +1,44 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+global $APPLICATION;
+
+if(empty($arResult))
+	return "";
+
+$strReturn = '<div class="text-banner__breadcrumbs d-flex gap-2">';
+
+$itemSize = count($arResult);
+for($index = 0; $index < $itemSize; $index++)
+{
+	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+	$arrow = ($index > 0 ? '
+        <svg class="icon size-s text-white-50 d-inline-block d-md-none" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+            <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-left"></use>
+        </svg>' : '');
+
+	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
+	{
+        $strReturn .= '
+            <a class="text-banner__breadcrumbs-item d-md-inline-flex align-items-center gap-2 text-s text-white-50 d-inline-flex" href="' . $arResult[$index]["LINK"] . '">
+                ' . $arrow . '
+                <span>' . $title . '</span>
+            </a>';
+	}
+	else
+	{
+		$strReturn .= '
+			<div class="text-banner__breadcrumbs-item d-md-inline-flex align-items-center gap-2 text-s text-white-50 d-inline-flex">
+                ' . $arrow . '
+                <span>' . $title . '</span>
+            </div>';
+	}
+}
+
+$strReturn .= '</div>';
+
+return $strReturn;
