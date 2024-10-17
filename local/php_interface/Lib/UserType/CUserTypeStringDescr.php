3436ecd212fbe25de6a803a6b18c5dd9cbcfd775
vimbatu <vimbatu@yandex.ru>
2024-10-17 21:12:33 +0300

    GLGNVKB-12 | изменяет кастомное свойство с несколькими полями. Добавлена возможность в настройках выбирать количество полей и подписи к ним. Подписи отображаются в плейсхолдерах полей в админке. Изменен способ хранения данных свойства в бд - json

--- local/php_interface/Lib/UserType/CUserTypeStringDescr.php
+++ local/php_interface/Lib/UserType/CUserTypeStringDescr.php
@@ -10,26 +10,29 @@ class CUserTypeStringDescr
     {
         return array(
             'PROPERTY_TYPE' => 'S',
-            "USER_TYPE" => 'triple_string', // уникальный ID
-            "DESCRIPTION" => 'Тройное строковое поле',
+            "USER_TYPE" => 'multiple_field',
+            "DESCRIPTION" => 'Множественное поле',
             'ConvertToDB' => [__CLASS__, 'ConvertToDB'],
             'ConvertFromDB' => [__CLASS__, 'ConvertFromDB'],
+            'GetSettingsHTML' => [__CLASS__, 'GetSettingsHTML'],
+            'PrepareSettings' => [__CLASS__, 'PrepareSettings'],
             'GetPropertyFieldHtml' => array(__CLASS__, 'GetPropertyFieldHtml'),
         );
     }
 
     public static function ConvertToDB($arProperty, $value)
     {
-        if (
-            !empty($value['VALUE']['FIRST'])
-            || !empty($value['VALUE']['SECOND'])
-            || !empty($value['VALUE']['THIRD'])
-        ) {
-            try {
-                $value['VALUE'] = base64_encode(serialize($value['VALUE']));
-            } catch (Bitrix\Main\ObjectException $exception) {
-                echo $exception->getMessage();
+        $hasValues = false;
+
+        for ($i = 1; $i <= $arProperty['USER_TYPE_SETTINGS']['COUNT']; $i++) {
+            if (!empty($value['VALUE'][$i])) {
+                $hasValues = true;
+                break;
             }
+        }
+
+        if ($hasValues) {
+            $value['VALUE'] = json_encode($value['VALUE']);
         } else {
             $value['VALUE'] = '';
         }
@@ -40,31 +43,122 @@ class CUserTypeStringDescr
     public static function ConvertFromDB($arProperty, $value, $format = '')
     {
         if ($value['VALUE'] != '') {
-            try {
-                $value['VALUE'] = base64_decode($value['VALUE']);
-            } catch (Bitrix\Main\ObjectException $exception) {
-                echo $exception->getMessage();
-            }
+            $value['VALUE'] = json_decode($value['VALUE'], true);
         }
 
         return $value;
     }
 
+    public static function GetSettingsHTML($arProperty, $strHTMLControlName, &$arPropertyFields)
+    {
+        $arPropertyFields = array(
+            "HIDE" => [
+                "SEARCHABLE",
+                "FILTRABLE",
+                "DEFAULT_VALUE"
+            ],
+            "SET" => ["FILTRABLE" => "N"],
+            "USER_TYPE_SETTINGS_TITLE" => "Настройки отображения свойства"
+        );
+
+        $html = '<tr>
+                    <td>Количество полей ввода:</td>
+                    <td><input type="text" size="3" id="input_count" name="' . $strHTMLControlName["NAME"] . '[COUNT]" value="' . ($arProperty["USER_TYPE_SETTINGS"]["COUNT"] ?? 1) . '" oninput="updateFields()"></td>
+                </tr>
+                <tr>
+                    <td>Подписи для полей:</td>
+                    <td id="field_descriptions">';
+
+        if (empty($arProperty["USER_TYPE_SETTINGS"]["DESCR"])) {
+            $html .= '<input type="text" size="20" name="' . $strHTMLControlName["NAME"] . '[DESCR][1]" value="Первое поле">';
+        } else {
+            $count = 1;
+            foreach ($arProperty["USER_TYPE_SETTINGS"]["DESCR"] as $value) {
+                $html .= '<input type="text" size="20" name="' . $strHTMLControlName["NAME"] . '[DESCR][' . $count . ']" value="' . $value . '"><br>';
+                $count++;
+            }
+        }
+
+        $html .= '</td></tr>';
+        $html .= '<script>
+                    var originalValues = {};
+
+                    function saveOriginalValues() {
+                        var existingFields = document.querySelectorAll("#field_descriptions input[type=\'text\']");
+                        existingFields.forEach(function(field) {
+                            var index = field.name.match(/\[(\d+)\]/)[1];
+                            originalValues[index] = field.value;
+                        });
+                    }
+
+                    function updateFields() {
+                        var count = parseInt(document.getElementById("input_count").value) || 1;
+                        var fieldDescriptions = document.getElementById("field_descriptions");
+                        fieldDescriptions.innerHTML = "";
+
+                        var numerals = ["Первое", "Второе", "Третье", "Четвертое", "Пятое", "Шестое", "Седьмое", "Восьмое", "Девятое", "Десятое"];
+
+                        for (var i = 1; i <= count; i++) {
+                            var descrText = originalValues[i] !== undefined ? originalValues[i] : numerals[i - 1] || (i + "-е поле");
+                            var inputHtml = "<input type=\'text\' size=\'20\' name=\'' . $strHTMLControlName["NAME"] . '[DESCR][" + i + "]\' value=\'" + descrText + "\'><br>";
+                            fieldDescriptions.innerHTML += inputHtml;
+                        }
+
+                        saveOriginalValues();
+                    }
+
+                    window.onload = function() {
+                        saveOriginalValues();
+                    };
+
+                    document.getElementById("input_count").addEventListener("input", updateFields);
+                </script>';
+
+
+        return $html;
+    }
+
+    public static function PrepareSettings($arFields)
+    {
+        $fields = [];
+        if (isset($arFields['USER_TYPE_SETTINGS']) && is_array($arFields['USER_TYPE_SETTINGS']))
+        {
+            $fields = $arFields['USER_TYPE_SETTINGS'];
+        }
+        $count = (int)($fields['COUNT'] ?? 1);
+        if ($count <= 0) {
+            $count = 1;
+        }
+        $description = $fields['DESCR'];
+        $numerals = ["Первое", "Второе", "Третье", "Четвертое", "Пятое", "Шестое", "Седьмое", "Восьмое", "Девятое", "Десятое"];
+
+        foreach ($description as $key => $value) {
+            if (!$value) {
+                $descrText = $numerals[$key] ?? ($key + 1) . "-е";
+                $description[$key] = $descrText;
+            }
+        }
+
+        return [
+            'COUNT' => $count,
+            'DESCR' => $description
+        ];
+    }
+
     public static function GetPropertyFieldHtml($arProperty, $value, $arHtmlControl)
     {
         $fieldName =  htmlspecialcharsbx($arHtmlControl['VALUE']);
-        $arValue = unserialize(htmlspecialcharsback($value['VALUE']), [stdClass::class]);
+        $arValue = $value['VALUE'];
 
-        $rate = $fieldName . "[FIRST]";
-        $amount = $fieldName . "[SECOND]";
-        $term = $fieldName . "[THIRD]";
+        $html = '';
 
-        $html = '<input name="' . $rate . '" id="' . $rate . '" value="' . htmlspecialcharsex($arValue['FIRST'] ?? '') . '" size="15" type="text">';
-        $html .= '<label for="' . $rate . '"> < Подпись </label>';
-        $html .= '<input name="' . $amount . '" id="' . $amount . '" value="' . htmlspecialcharsex($arValue['SECOND'] ?? '') . '" size="15" type="text">';
-        $html .= '<label for="' . $amount . '"> < Мелко </label>';
-        $html .= '<input name="' . $term . '" id="' . $term . '" value="' . htmlspecialcharsex($arValue['THIRD'] ?? '') . '" size="15" type="text">';
-        $html .= '<label for="' . $term . '"> < Крупно </label>';
+        for ($i = 1; $i <= $arProperty['USER_TYPE_SETTINGS']['COUNT']; $i++) {
+            $name = $fieldName . '[' . $i . ']';
+            $currentValue = htmlspecialcharsex($arValue[$i] ?? '');
+            $description = $arProperty['USER_TYPE_SETTINGS']['DESCR'][$i];
+
+            $html .= '<input name="' . $name . '" id="' . $name . '" value="' . $currentValue . '" size="15" type="text" placeholder="' . $description . '">';
+        }
 
         return $html;
     }
