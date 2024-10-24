c7e6886e19a58d4c133ce3aed2493481b402679c
Ilya Ponomarev <admin@iMac-admin.local>
2024-10-24 21:28:46 +0500

    GLGNVKB-6 | добавит стили для стандартных bootstrap компонентов

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -31,6 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.onload = function() {
     initPolygonContainer();
+
+    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 };
 
 window.addEventListener('resize', () => {
