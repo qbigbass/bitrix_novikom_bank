c5435e804aa69ddd9a44cc9666dc8758805e4256
Mark Kim <mark.kim@dalee.ru>
2024-10-25 17:18:46 +0300

    master | fix including resources

--- frontend-astro/src/components/common/CalculatorCard/index.ts
+++
@@ -1,6 +0,0 @@
-//TODO
-// 1) В будущем написать API компонентов CalculatorCard и CalculatorCardItem;
-// 2) Для CalculatorCard нужно написать работу:
-//    1.1) С методами CalculatorCardItem;
-//    1.2) Реагирование нажатия на кнопку 'Оформить заявку';
-// 3) Для CalculatorCardItem нужно написать методы для работы с контентом внутри;
