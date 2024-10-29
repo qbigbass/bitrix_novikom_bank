0a09a18835bd4d4e1bc70e33ecb371e28f470a2c
vimbatu <vimbatu@yandex.ru>
2024-10-29 13:00:03 +0300

    GLGNVKB-12 | добавляет к компоненту бенефитов параметр для выбора количества колонок

--- local/templates/main/components/bitrix/news.list/benefits/template.php
+++ local/templates/main/components/bitrix/news.list/benefits/template.php
@@ -20,13 +20,15 @@ $this->setFrameMode(true);?>
         <div class="row row-gap-6">
 
             <? foreach ($arResult['ITEMS'] as $benefit) { ?>
-                <div class="col-12 col-md-6 col-lg-4">
+                <div class="col-12 col-md-6 col-lg-<?= 12 / $arParams['COL_COUNT']?>">
                     <div class="benefit d-flex gap-3 flex-column">
                         <img class="icon size-xl" src="<?= $benefit['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $benefit['PREVIEW_PICTURE']['ALT'] ?>" loading="lazy">
                         <div class="benefit__content d-flex flex-column gap-3">
-                            <h4 class="benefit__title"><?= $benefit['~NAME'] ?></h4>
+                            <? if ($benefit['~PREVIEW_TEXT']) { ?>
+                                <h4 class="benefit__title"><?= $benefit['~NAME'] ?></h4>
+                            <? } ?>
                             <span class="benefit__description w-100 w-md-75 text-m">
-                                <?= $benefit['~PREVIEW_TEXT'] ?>
+                                <?= $benefit['~PREVIEW_TEXT'] ?: $benefit['~NAME'] ?>
                             </span>
                         </div>
                     </div>
