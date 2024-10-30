70bfebb3b3b040c88faa6c7cc976321aa243826d
Mark Kim <mark.kim@dalee.ru>
2024-10-30 17:11:10 +0300

    master | fix namespace

--- local/templates/main/components/bitrix/news.detail/loans_detail/result_modifier.php
+++ local/templates/main/components/bitrix/news.detail/loans_detail/result_modifier.php
@@ -1,7 +1,7 @@
 <?php
 /** @var array $arResult */
 
-use Lib\Dalee\Classes\RatesFetcher;
+use Dalee\Helpers\RatesFetcher;
 
 $ratesFetcher = new RatesFetcher(iblock($arResult['IBLOCK_CODE'] . '_rates'));
 $ratesFetcher->fetchRates($arResult['ID']);
