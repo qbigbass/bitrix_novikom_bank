8de06d8b765467b2eec6c3e06d560f683cac29f2
persol <persol2013@gmail.com>
2024-11-05 12:59:06 +0500

    DNVKBSITE-36 | добавит плагин "air-datepicker"

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -11,6 +11,7 @@ import initSelect2 from './components/select2.js'
 import { initTabsContent } from './components/tabs.js'
 import initInputSlider from "./components/inputSlider";
 import showMoreContent from "./components/showMoreContent";
+import initDatepicker from './components/datepicker.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     initDropdownMenu();
@@ -27,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     initTabsContent();
     initInputSlider();
     showMoreContent();
+    initDatepicker();
 });
 
 window.onload = function() {
