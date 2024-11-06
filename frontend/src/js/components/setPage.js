b44fd7f5b7cfc26d19aa501354a50aee9c77857d
Kseniya Vasilyeva <kseniya.vasileva@dalee.dev>
2024-11-06 14:07:58 +0400

    DNVKBSITE-50 | поправит скрипт setPage с then на async/await

--- frontend/src/js/components/setPage.js
+++ frontend/src/js/components/setPage.js
@@ -4,33 +4,36 @@ const TABS_DATA = {
     buttons: '[data-tab-button-page]',
 }
 
-function setPage() {
+async function setPage() {
     const $buttons = $(TABS_DATA.buttons);
 
     if (!$buttons.length) {
         return false;
     }
 
-    $buttons.on('click', function() {
+    $buttons.on('click', async function () {
         const page = $(this).data('tab-button-page');
 
-        fetch('/local/php_interface/ajax/ajax_set_page.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams({
-                action: 'set_page',
-                page: page
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 'success') {
-                    location.reload();
-                }
-            })
-            .catch(error => console.error('Error:', error));
+        try {
+            const response = await fetch('/local/php_interface/ajax/ajax_set_page.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    action: 'set_page',
+                    page: page
+                })
+            });
+
+            const data = await response.json();
+
+            if (data.status === 'success') {
+                location.reload();
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
 }
 
