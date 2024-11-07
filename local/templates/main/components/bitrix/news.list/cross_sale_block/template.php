b9f4db903fdc6ace26ddb2773cf7da3449e9ac85
vimbatu <vimbatu@yandex.ru>
2024-11-07 15:33:04 +0300

    GLGNVKB-12 | правки по блоку перекрестных продаж

--- local/templates/main/components/bitrix/news.list/cross_sale_block/template.php
+++ local/templates/main/components/bitrix/news.list/cross_sale_block/template.php
@@ -22,10 +22,14 @@ $this->setFrameMode(true);
                     <div class="swiper-slide js-swiper-slide">
                         <div class="card-product card-product--<?= $item['PROPERTIES']['LINE_COLOR']['VALUE_XML_ID'] ?>">
                             <div class="card-product__inner">
-                                <div class="tag card-product__tag"><span class="tag__content text-s fw-semibold"><?= $item['PROPERTIES']['TAG']['VALUE'] ?></span><span class="tag__triangle">
-                                    <svg width="14" height="21" viewBox="0 0 14 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M13.5 19.3486L0.934259 0.5H13.5V19.3486Z"></path>
-                                    </svg></span></div>
+                                <div class="tag card-product__tag">
+                                    <span class="tag__content text-s fw-semibold"><?= $item['PROPERTIES']['TAG']['VALUE'] ?></span>
+                                    <span class="tag__triangle">
+                                        <svg width="14" height="21" viewBox="0 0 14 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M13.5 19.3486L0.934259 0.5H13.5V19.3486Z"></path>
+                                        </svg>
+                                    </span>
+                                </div>
                                 <div class="card-product__content">
                                     <h4 class="card-product__title"><?= $item['~NAME'] ?></h4>
                                     <? if(!empty($item['PROPERTIES']['CONDITION']['VALUE'])) { ?>
@@ -39,9 +43,18 @@ $this->setFrameMode(true);
                                     <? } ?>
                                 </div>
                                 <img class="card-product__img" src="<?= $item['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $item['PREVIEW_PICTURE']['ALT'] ?>" loading="lazy">
-                                <a class="btn btn-primary card-product__button" href="<?= $item['PROPERTIES']['LINK']['VALUE'] ?>">
-                                    <?= $item['PROPERTIES']['BTN_TEXT']['VALUE'] ?>
-                                </a>
+                                <? if (empty($item['PROPERTIES']['BTN_TYPE']['VALUE']) || $item['PROPERTIES']['BTN_TYPE']['VALUE'] == 'С фоном') { ?>
+                                    <a class="btn btn-primary card-product__button" href="<?= $item['PROPERTIES']['LINK']['VALUE'] ?>">
+                                        <?= $item['PROPERTIES']['BTN_TEXT']['VALUE'] ?>
+                                    </a>
+                                <? } else { ?>
+                                    <a class="btn btn-link btn-icon m-auto m-md-0" href="<?= $item['PROPERTIES']['LINK']['VALUE'] ?>">
+                                        <span><?= $item['PROPERTIES']['BTN_TEXT']['VALUE'] ?></span>
+                                        <svg class="icon size-m" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                            <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-right"></use>
+                                        </svg>
+                                    </a>
+                                <? } ?>
                             </div>
                         </div>
                     </div>
@@ -50,14 +63,20 @@ $this->setFrameMode(true);
             <div class="slider-controls js-swiper-controls mt-3 mt-md-4">
                 <div class="slider-controls__pagination js-swiper-pagination"></div>
                 <div class="slider-controls__navigation js-swiper-nav">
-                    <button class="swiper-button-prev js-swiper-prev" type="button"><span class="icon size-m">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-                                    <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-left"></use>
-                                  </svg></span></button>
-                    <button class="swiper-button-next js-swiper-next" type="button"><span class="icon size-m">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-                                    <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-right"></use>
-                                  </svg></span></button>
+                    <button class="swiper-button-prev js-swiper-prev" type="button">
+                        <span class="icon size-m">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-left"></use>
+                            </svg>
+                        </span>
+                    </button>
+                    <button class="swiper-button-next js-swiper-next" type="button">
+                        <span class="icon size-m">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-right"></use>
+                            </svg>
+                        </span>
+                    </button>
                 </div>
             </div>
         </div>
