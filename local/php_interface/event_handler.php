6ebe402622852992b734673f29c55b102778bf74
vimbatu <vimbatu@yandex.ru>
2024-11-12 17:20:58 +0300

    GLGNVKB-12 | добавляет кастомное свойство для вывода сгруппированных свойств

--- local/php_interface/event_handler.php
+++ local/php_interface/event_handler.php
@@ -3,8 +3,20 @@
 use Bitrix\Main;
 use Dalee\UserType\CUserTypeStringDescr;
 use Dalee\UserType\CUserTypeStringWithTabs;
+use Dalee\UserType\CUserTypeComplexProperty;
+use Dalee\UserType\CIBEditComplexProp;
 
 $eventManager = Main\EventManager::getInstance();
 
 $eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', [CUserTypeStringDescr::class, 'OnIBlockPropertyBuildList']);
 $eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', [CUserTypeStringWithTabs::class, 'OnIBlockPropertyBuildList']);
+$eventManager->addEventHandler('iblock', 'OnIBlockPropertyBuildList', [CUserTypeComplexProperty::class, 'OnIBlockPropertyBuildList']);
+
+$eventManager->addEventHandler('iblock', 'OnStartIBlockElementAdd', [CIBEditComplexProp::class, 'OnStartIBlockElementUpdateHandler']);
+$eventManager->addEventHandler('iblock', 'OnStartIBlockElementUpdate', [CIBEditComplexProp::class, 'OnStartIBlockElementUpdateHandler']);
+$eventManager->addEventHandler('iblock', 'OnBeforeIBlockElementAdd', [CIBEditComplexProp::class, 'OnBeforeIBlockElementUpdateHandler']);
+$eventManager->addEventHandler('iblock', 'OnBeforeIBlockElementUpdate', [CIBEditComplexProp::class, 'OnBeforeIBlockElementUpdateHandler']);
+$eventManager->addEventHandler('iblock', 'OnAfterIBlockElementAdd', [CIBEditComplexProp::class, 'OnAfterIBlockElementUpdateHandler']);
+$eventManager->addEventHandler('iblock', 'OnAfterIBlockElementUpdate', [CIBEditComplexProp::class, 'OnAfterIBlockElementUpdateHandler']);
+
+$eventManager->addEventHandler('main', 'OnBeforeProlog', [CIBEditComplexProp::class, 'OnBeforePrologHandler']);
