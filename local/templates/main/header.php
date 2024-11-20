dd502204cd7febbbdf65783ed09fdecf61e89aa6
Mark Kim <mark.kim@dalee.ru>
2024-11-21 02:13:10 +0300

    DNVKBSITE-36 | rebased on to master

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -9,8 +9,6 @@ use Bitrix\Main\Page\Asset;
 $asset = Asset::getInstance();
 
 $asset->addCss('/frontend/dist/css/bootstrap.css');
-$asset->addCss('/frontend/dist/css/select2.css');
-$asset->addCss('/frontend/dist/css/air-datepicker.css');
 $asset->addCss('/frontend/dist/css/swiper.css');
 $asset->addCss('/frontend/dist/css/select2.css');
 $asset->addCss('/frontend/dist/css/air-datepicker.css');
@@ -21,7 +19,6 @@ $asset->addJs('/frontend/dist/js/popover.js');
 $asset->addJs('/frontend/dist/js/bootstrap.min.js');
 $asset->addJs('/frontend/dist/js/swiper.min.js');
 $asset->addJs('/frontend/dist/js/select2.min.js');
-$asset->addJs('/frontend/dist/js/airdatepicker.js');
 $asset->addJs('/frontend/dist/js/airdatepicker.js');
 $asset->addJs('/frontend/dist/js/bundle.js');
 ?>
