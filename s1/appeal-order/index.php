45eaebb875b44801f8e95119160a41536e446fe7
vimbatu <vimbatu@yandex.ru>
2024-11-21 14:46:39 +0300

    GLGNVKB-12 | добавляет порядок обращения в банк

---
+++ s1/appeal-order/index.php
@@ -0,0 +1,285 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+global $APPLICATION;
+$APPLICATION->SetTitle("Порядок обращения<br> в банк");
+
+use Dalee\Helpers\HeaderView;
+
+$headerView = new HeaderView();
+
+$headerView->render(
+    $APPLICATION->GetTitle(),
+    null,
+    ['bg-linear-blue', 'banner-text--border-green']
+);
+?>
+
+<?
+$elementIds = getIblockSectionElementsIds('benefits', 'obshchie');
+if (!empty($elementIds)) { ?>
+
+    <section class="section-layout px-lg-6">
+        <div class="container">
+            <div class="row row-gap-6 gx-xl-6">
+                <? global $benefitsFilter;
+                $benefitsFilter = [
+                    'ACTIVE' => 'Y',
+                    'ID' => getIblockSectionElementsIds('benefits', 'obshchie')
+                ];
+
+                $APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    "benefits",
+                    [
+                        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                        "ADD_SECTIONS_CHAIN" => "N",
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_ADDITIONAL" => "",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "Y",
+                        "CACHE_FILTER" => "Y",
+                        "CACHE_GROUPS" => "Y",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_TYPE" => "A",
+                        "CHECK_DATES" => "Y",
+                        "COL_COUNT" => "2",
+                        "DETAIL_URL" => "",
+                        "DISPLAY_BOTTOM_PAGER" => "N",
+                        "DISPLAY_TOP_PAGER" => "N",
+                        "FIELD_CODE" => ["CODE", "NAME", "PREVIEW_TEXT", "PREVIEW_PICTURE", ""],
+                        "FILTER_NAME" => "benefitsFilter",
+                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                        "IBLOCK_ID" => iblock('benefits'),
+                        "IBLOCK_TYPE" => "additional",
+                        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                        "INCLUDE_SUBSECTIONS" => "N",
+                        "MESSAGE_404" => "",
+                        "NEWS_COUNT" => "20",
+                        "PAGER_BASE_LINK_ENABLE" => "N",
+                        "PAGER_DESC_NUMBERING" => "N",
+                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                        "PAGER_SHOW_ALL" => "N",
+                        "PAGER_SHOW_ALWAYS" => "N",
+                        "PAGER_TEMPLATE" => ".default",
+                        "PAGER_TITLE" => "Новости",
+                        "PARENT_SECTION" => "",
+                        "PARENT_SECTION_CODE" => "",
+                        "PREVIEW_TRUNCATE_LEN" => "",
+                        "PROPERTY_CODE" => ["", ""],
+                        "SET_BROWSER_TITLE" => "N",
+                        "SET_LAST_MODIFIED" => "N",
+                        "SET_META_DESCRIPTION" => "N",
+                        "SET_META_KEYWORDS" => "N",
+                        "SET_STATUS_404" => "N",
+                        "SET_TITLE" => "N",
+                        "SHOW_404" => "N",
+                        "SORT_BY1" => "ACTIVE_FROM",
+                        "SORT_BY2" => "SORT",
+                        "SORT_ORDER1" => "DESC",
+                        "SORT_ORDER2" => "ASC",
+                        "STRICT_SECTION_CHECK" => "N",
+                    ],
+                    false,
+                    ['HIDE_ICONS' => 'Y']
+                ); ?>
+            </div>
+        </div>
+        <picture class="pattern-bg pattern-bg--position-sm-bottom section-restructuring-benefits__pattern">
+            <source srcset="/frontend/dist/img/patterns/section/pattern-light-s.svg" media="(max-width: 767px)">
+            <source srcset="/frontend/dist/img/patterns/section/pattern-light-m.svg" media="(max-width: 1199px)">
+            <img src="/frontend/dist/img/patterns/section/pattern-light-l.svg" alt="bg pattern" loading="lazy">
+        </picture>
+    </section>
+<? } ?>
+
+<?
+$elementsInfoIds = getIblockSectionElementsIds('benefits', 'informatsiya-dlya-obrashcheniya');
+if (!empty($elementsInfoIds)) { ?>
+    <section class="section-layout px-lg-6">
+        <div class="container">
+            <h3 class="mb-4 mb-md-6 mb-lg-7">
+                <? $APPLICATION->IncludeFile('/appeal-order/include_header_info.php'); ?>
+            </h3>
+            <div class="row row-gap-6 gx-xl-6">
+                <? global $benefitsFilter;
+                $benefitsFilter = [
+                    'ACTIVE' => 'Y',
+                    'ID' => $elementsInfoIds
+                ];
+
+                $APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    "benefits",
+                    [
+                        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                        "ADD_SECTIONS_CHAIN" => "N",
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_ADDITIONAL" => "",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "Y",
+                        "CACHE_FILTER" => "Y",
+                        "CACHE_GROUPS" => "Y",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_TYPE" => "A",
+                        "CHECK_DATES" => "Y",
+                        "COL_COUNT" => "2",
+                        "DETAIL_URL" => "",
+                        "DISPLAY_BOTTOM_PAGER" => "N",
+                        "DISPLAY_TOP_PAGER" => "N",
+                        "FIELD_CODE" => ["CODE", "NAME", "PREVIEW_TEXT", "PREVIEW_PICTURE", ""],
+                        "FILTER_NAME" => "benefitsFilter",
+                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                        "IBLOCK_ID" => iblock('benefits'),
+                        "IBLOCK_TYPE" => "additional",
+                        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                        "INCLUDE_SUBSECTIONS" => "N",
+                        "MESSAGE_404" => "",
+                        "NEWS_COUNT" => "20",
+                        "PAGER_BASE_LINK_ENABLE" => "N",
+                        "PAGER_DESC_NUMBERING" => "N",
+                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                        "PAGER_SHOW_ALL" => "N",
+                        "PAGER_SHOW_ALWAYS" => "N",
+                        "PAGER_TEMPLATE" => ".default",
+                        "PAGER_TITLE" => "Новости",
+                        "PARENT_SECTION" => "",
+                        "PARENT_SECTION_CODE" => "",
+                        "PREVIEW_TRUNCATE_LEN" => "",
+                        "PROPERTY_CODE" => ["", ""],
+                        "SET_BROWSER_TITLE" => "N",
+                        "SET_LAST_MODIFIED" => "N",
+                        "SET_META_DESCRIPTION" => "N",
+                        "SET_META_KEYWORDS" => "N",
+                        "SET_STATUS_404" => "N",
+                        "SET_TITLE" => "N",
+                        "SHOW_404" => "N",
+                        "SORT_BY1" => "ACTIVE_FROM",
+                        "SORT_BY2" => "SORT",
+                        "SORT_ORDER1" => "DESC",
+                        "SORT_ORDER2" => "ASC",
+                        "STRICT_SECTION_CHECK" => "N",
+                    ],
+                    false,
+                    ['HIDE_ICONS' => 'Y']
+                ); ?>
+            </div>
+        </div>
+        <picture class="pattern-bg pattern-bg--position-sm-bottom section-restructuring-benefits__pattern">
+            <source srcset="/frontend/dist/img/patterns/section/pattern-light-s.svg" media="(max-width: 767px)">
+            <source srcset="/frontend/dist/img/patterns/section/pattern-light-m.svg" media="(max-width: 1199px)">
+            <img src="/frontend/dist/img/patterns/section/pattern-light-l.svg" alt="bg pattern" loading="lazy">
+        </picture>
+    </section>
+<? } ?>
+
+<section class="section-layout bg-dark-10 px-lg-6">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="polygon-container js-polygon-container">
+                    <div class="polygon-container__content">
+                        <div class="helper bg-white">
+                            <div
+                                class="helper__wrapper d-flex flex-column flex-sm-row align-items-start align-items-sm-center gap-4 gap-lg-6">
+                                <img class="helper__image w-auto float-end"
+                                     src="/frontend/dist/img/restructuring-additional-info.png" alt="Обратите внимание"
+                                     loading="lazy">
+                                <div class="helper__content text-l">
+                                    <p class="mb-0">
+                                        <? $APPLICATION->IncludeFile('/appeal-order/quote_info.php'); ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="polygon-container__polygon js-polygon-container-polygon violet-100">
+                        <svg class="js-polygon-container-svg" xmlns="http://www.w3.org/2000/svg">
+                            <polygon points="2,2 335,2 335,394 295,434 2,434" fill="none" stroke="currentColor"
+                                     stroke-width="2" stroke-dasharray="10"></polygon>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?
+$elementsTabs = getIblockSectionElementsIds('tabs', 'poryadok-obrashcheniya-v-bank');
+if (!empty($elementsInfoIds)) { ?>
+
+    <section class="section-layout px-lg-6">
+        <div class="container">
+            <h3 class="mb-md-6 mb-lg-7"><? $APPLICATION->IncludeFile('/appeal-order/tabs_header.php'); ?></h3>
+
+            <? global $tabsFilter;
+            $tabsFilter = [
+                'ACTIVE' => 'Y',
+                'ID' => $elementsTabs
+            ];
+
+            $APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "tabs",
+                [
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "ADD_SECTIONS_CHAIN" => "N",
+                    "AJAX_MODE" => "N",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "CACHE_FILTER" => "Y",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_TYPE" => "A",
+                    "CHECK_DATES" => "Y",
+                    "DETAIL_URL" => "",
+                    "DISPLAY_BOTTOM_PAGER" => "N",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "FIELD_CODE" => ["CODE", "NAME", "PREVIEW_TEXT", "PREVIEW_PICTURE", ""],
+                    "FILTER_NAME" => "tabsFilter",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "IBLOCK_ID" => iblock('tabs'),
+                    "IBLOCK_TYPE" => "additional",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                    "INCLUDE_SUBSECTIONS" => "N",
+                    "MESSAGE_404" => "",
+                    "NEWS_COUNT" => "20",
+                    "PAGER_BASE_LINK_ENABLE" => "N",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "N",
+                    "PAGER_SHOW_ALWAYS" => "N",
+                    "PAGER_TEMPLATE" => ".default",
+                    "PAGER_TITLE" => "Новости",
+                    "PARENT_SECTION" => "",
+                    "PARENT_SECTION_CODE" => "",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "PROPERTY_CODE" => ["CONDITIONS_ICONS", "CONDITIONS", "CONDITIONS_TABS", "TEXT_FIELD", "SHORT_INFO", "QUOTES", "QUESTIONS", "DOCUMENTS"],
+                    "SET_BROWSER_TITLE" => "N",
+                    "SET_LAST_MODIFIED" => "N",
+                    "SET_META_DESCRIPTION" => "N",
+                    "SET_META_KEYWORDS" => "N",
+                    "SET_STATUS_404" => "N",
+                    "SET_TITLE" => "N",
+                    "SHOW_404" => "N",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_ORDER2" => "ASC",
+                    "STRICT_SECTION_CHECK" => "N",
+                    "NO_TABS_PADDING" => true,
+                ],
+                false,
+                ['HIDE_ICONS' => 'Y']
+            ); ?>
+
+        </div>
+    </section>
+
+<? } ?>
+
+<? require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
