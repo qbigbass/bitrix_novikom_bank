244ebbde779958a7be493c8e2b177105f870d96a
Ilya Ponomarev <admin@iMac-admin.local>
2024-11-21 12:28:13 +0500

    DNVBSITE-82 | добавит страницы Офисы и банкоматы

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -18,6 +18,8 @@ import {initFormFeedback} from "./components/form/formFeedback";
 import {initFormSend} from "./components/form/formSend";
 import {initUploadFile} from "./components/form/uploadFile";
 import {initMask} from "./components/form/mask";
+import initOffices from "./components/offices";
+import initYMap from "./components/yMap";
 
 document.addEventListener('DOMContentLoaded', () => {
     initDropdownMenu();
@@ -41,6 +43,8 @@ document.addEventListener('DOMContentLoaded', () => {
     initFormSend();
     initUploadFile();
     initMask();
+    initOffices();
+    initYMap();
 });
 
 window.onload = function() {
