ee782badae6a8ae7f16c455c6c851a0fb7904fc4
Вотинцев Андрей Александрович <votincev-aa@galagodigital.ru>
2024-11-29 18:45:07 +0500

    DNVKBSITE-34 добавляет меню для раздела услуг

---
+++ s1/services/.iblock_sections.menu.php
@@ -0,0 +1,42 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$aMenuLinks = [
+    [
+        'SMS-информирование',
+        '/services/sms-informirovanie/',
+        [],
+        []
+    ],
+    [
+        'Биометрическая идентификация',
+        '/services/biometricheskaya-identifikatsiya/',
+        [],
+        []
+    ],
+    [
+        'Интернет-банк и Мобильное приложение',
+        '/services/internet-bank-i-mobilnoe-prilozhenie/',
+        [],
+        []
+    ],
+    [
+        'Перечень страховых компаний',
+        '/services/perechen-strakhovykh-kompaniy/',
+        [],
+        []
+    ],
+    [
+        'Программы страхования',
+        '/services/programmy-strakhovaniya/',
+        [],
+        []
+    ],
+    [
+        'Прочие сервисы',
+        '/services/prochie-servisy/',
+        [],
+        []
+    ],
+];
+
+?>
