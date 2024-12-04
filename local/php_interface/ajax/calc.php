5d5bf76c15ab46c616b20207c1393fec80367740
vimbatu <vimbatu@yandex.ru>
2024-12-04 14:18:51 +0300

    GLGNVKB-12 | добавляет класс для обработки апи запроса на получение ставок для калькуляторов. Также изменил название иб program_bonuses_rates_ru на program_bonuses_rates

---
+++ local/php_interface/ajax/calc.php
@@ -0,0 +1,12 @@
+<?php
+use Dalee\Services\ProductRatesHandler;
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+$data = $_POST;
+
+$table = $data['table'];
+$elementId = $data['elementId'] ?? null;
+$name = $data['name'] ?? null;
+
+$handler = new ProductRatesHandler($table, $elementId, $name);
+$handler->json();
