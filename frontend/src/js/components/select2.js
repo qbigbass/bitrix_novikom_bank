1648f44503173254070f9d51d941220a9a8c802e
Kseniya Vasilyeva <kseniya.vasileva@dalee.dev>
2024-12-06 09:24:27 +0400

    DNVKBSITE-107 | пофиксит select2

--- frontend/src/js/components/select2.js
+++ frontend/src/js/components/select2.js
@@ -1,5 +1,5 @@
 // import $ from "../vendors/jquery.min";
-import $ from "jquery";
+// import $ from "jquery";
 // import "select2/dist/css/select2.css";
 import select2 from "../vendors/select2.min";
 
