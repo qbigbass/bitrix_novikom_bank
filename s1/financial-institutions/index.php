8507c4c67e2868354c9b3f08b6f30b8b009c202e
Radik.M <r.machmutov@astarus.com>
2024-12-09 18:35:19 +0400

    DNVKBSITE-122 | Добавил блок "Объявления для клиентов" на главную МСБ. Поправил кол-во объявлений на главной для Финансовых институтов.

--- s1/financial-institutions/index.php
+++ s1/financial-institutions/index.php
@@ -239,7 +239,7 @@ $APPLICATION->IncludeComponent(
         "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
         "INCLUDE_SUBSECTIONS" => "N",
         "MESSAGE_404" => "",
-        "NEWS_COUNT" => "2",
+        "NEWS_COUNT" => "20",
         "PAGER_BASE_LINK_ENABLE" => "N",
         "PAGER_DESC_NUMBERING" => "N",
         "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
