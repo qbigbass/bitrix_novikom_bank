78fbadcc7bd023230f8c85d15a83780f1e8d8d42
Requiem <johndoe@example.com>
2024-12-13 14:08:39 +0400

    version for see-disabled people

--- frontend/src/js/components/accessibility-panel.js
+++ frontend/src/js/components/accessibility-panel.js
@@ -18,6 +18,8 @@ const MEDIUM_FONT_SIZE = 1.25;
 const LARGE_FONT_SIZE = 1.5;
 
 $(document).ready(function () {
+  const savedFontSize = localStorage.getItem("accessibilityFontSize");
+  const savedContrast = localStorage.getItem("accessibilityContrast");
   const isPanelOpen = localStorage.getItem("accessibilityPanelOpen") === "true";
 
   if (isPanelOpen) {
@@ -26,43 +28,40 @@ $(document).ready(function () {
     closeAccessibilityPanel();
   }
 
-  $(".contrastColorsBlue").trigger("click");
-  $(PANEL_IDS.fontDefault).trigger("click");
+  if (savedFontSize) {
+    applyFontSize(savedFontSize);
+  } else {
+    $(PANEL_IDS.fontDefault).trigger("click");
+  }
+
+  if (savedContrast) {
+    applyContrast(savedContrast);
+  } else {
+    $(PANEL_IDS.contrastColorsBlue).trigger("click");
+  }
 
   $(PANEL_IDS.contrastColorsBlue).click(function () {
-    $("body")
-      .removeClass("blue-contrast black-contrast yellow-contrast")
-      .addClass("blue-contrast");
-    updateActiveContrastButton(this);
+    applyContrast("blue-contrast");
   });
 
   $(PANEL_IDS.contrastColorsBlack).click(function () {
-    $("body")
-      .removeClass("blue-contrast yellow-contrast")
-      .addClass("black-contrast");
-    updateActiveContrastButton(this);
+    applyContrast("black-contrast");
   });
 
   $(PANEL_IDS.contrastColorsYellow).click(function () {
-    $("body")
-      .removeClass("blue-contrast black-contrast")
-      .addClass("yellow-contrast");
-    updateActiveContrastButton(this);
+    applyContrast("yellow-contrast");
   });
 
   $(PANEL_IDS.fontDefault).click(function () {
-    $("html").css("font-size", `${DEFAULT_FONT_SIZE}rem`);
-    updateActiveButton(PANEL_IDS.fontDefault);
+    applyFontSize(DEFAULT_FONT_SIZE);
   });
 
   $(PANEL_IDS.fontMedium).click(function () {
-    $("html").css("font-size", `${MEDIUM_FONT_SIZE}rem`);
-    updateActiveButton(PANEL_IDS.fontMedium);
+    applyFontSize(MEDIUM_FONT_SIZE);
   });
 
   $(PANEL_IDS.fontLarge).click(function () {
-    $("html").css("font-size", `${LARGE_FONT_SIZE}rem`);
-    updateActiveButton(PANEL_IDS.fontLarge);
+    applyFontSize(LARGE_FONT_SIZE);
   });
 
   $(PANEL_IDS.showAccessibilityPanel).click(function () {
@@ -73,6 +72,20 @@ $(document).ready(function () {
     closeAccessibilityPanel();
   });
 
+  function applyFontSize(fontSize) {
+    $("html").css("font-size", `${fontSize}rem`);
+    localStorage.setItem("accessibilityFontSize", fontSize);
+    updateActiveButton(getFontButtonSelector(fontSize));
+  }
+
+  function applyContrast(contrastClass) {
+    $("body")
+      .removeClass("blue-contrast black-contrast yellow-contrast")
+      .addClass(contrastClass);
+    localStorage.setItem("accessibilityContrast", contrastClass);
+    updateActiveContrastButton(getContrastButtonSelector(contrastClass));
+  }
+
   function openAccessibilityPanel() {
     $(PANEL_IDS.accessibilityPanel).slideDown();
     $("body").css("margin-top", 100);
@@ -82,13 +95,35 @@ $(document).ready(function () {
 
   function closeAccessibilityPanel() {
     $(PANEL_IDS.accessibilityPanel).slideUp();
-    $("html").css("font-size", `${DEFAULT_FONT_SIZE}rem`);
     $("body").removeClass(
       "accessibility blue-contrast black-contrast yellow-contrast"
     );
+    $("html").css("font-size", `${DEFAULT_FONT_SIZE}rem`);
     $("body").css("margin-top", 0);
     $("img").show();
     localStorage.setItem("accessibilityPanelOpen", "false");
+  }
+
+  function getFontButtonSelector(fontSize) {
+    switch (parseFloat(fontSize)) {
+      case MEDIUM_FONT_SIZE:
+        return PANEL_IDS.fontMedium;
+      case LARGE_FONT_SIZE:
+        return PANEL_IDS.fontLarge;
+      default:
+        return PANEL_IDS.fontDefault;
+    }
+  }
+
+  function getContrastButtonSelector(contrastClass) {
+    switch (contrastClass) {
+      case "black-contrast":
+        return PANEL_IDS.contrastColorsBlack;
+      case "yellow-contrast":
+        return PANEL_IDS.contrastColorsYellow;
+      default:
+        return PANEL_IDS.contrastColorsBlue;
+    }
   }
 
   function updateActiveButton(activeButton) {
