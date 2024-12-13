b7fc7debf9e4c0eebd6daa4f4cbbe06531336d3b
Radik.M <r.machmutov@astarus.com>
2024-12-13 16:54:32 +0400

    DNVKBSITE-134 | Добавил формы в футер для чатбота.

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -289,10 +289,25 @@ $APPLICATION->IncludeComponent(
     "",
     [
         "FORM_TITLES" => ["Заказать звонок", "Направить обращение"],
-        "FORM_CODES" => ["modal-callback-form", "modal-feedback_form"],
+        "FORM_CODES" => ["modal-callback-form", "modal-feedback-form"],
         "FORM_ICONS" => ["img/svg-sprite.svg#icon-phone", "img/svg-sprite.svg#icon-mail"],
     ]
 );
 ?>
+<?php $APPLICATION->IncludeComponent(
+    "dalee:form",
+    "callback_form",
+    [
+        "FORM_CODE" => "callback_form",
+    ]
+); ?>
+
+<?php $APPLICATION->IncludeComponent(
+    "dalee:form",
+    "feedback_form",
+    [
+        "FORM_CODE" => "feedback_form",
+    ]
+); ?>
 </body>
 </html>
