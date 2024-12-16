564e874fa142f377bd40de65b78d28540843afed
Kseniya Vasilyeva <kseniya.vasileva@dalee.dev>
2024-12-16 10:01:55 +0400

    DNVKBSITE-61 | доработка блока обратного звонка

--- local/php_interface/include/request_call.php
+++ local/php_interface/include/request_call.php
@@ -8,7 +8,7 @@
                 </div>
             </div>
             <div class="d-flex flex-column align-items-start gap-5 gap-md-6 ps-lg-6">
-                <button class="btn btn-info w-100 w-lg-auto" type="button">Направить обращение</button>
+                <button class="btn btn-info w-100 w-lg-auto" type="button" data-bs-toggle="modal" data-bs-target="#modal-feedback-form">Направить обращение</button>
                 <p class="text-s dark-70 mb-0">Нажимая кнопку «Перезвоните мне», вы&nbsp;соглашаетесь с&nbsp;условиями предоставления информации</p>
             </div>
             <?php $APPLICATION->IncludeComponent(
