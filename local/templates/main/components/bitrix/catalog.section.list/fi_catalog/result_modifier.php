95045160b034fcb97aa39d9ebcac7001970904c4
Radik.M <r.machmutov@astarus.com>
2024-12-19 11:34:35 +0400

    DNVKBSITE-136 | Поправил логику обработки тегов в нижнем меню, на главной странице "Финансовым институтам".

--- local/templates/main/components/bitrix/catalog.section.list/fi_catalog/result_modifier.php
+++ local/templates/main/components/bitrix/catalog.section.list/fi_catalog/result_modifier.php
@@ -53,6 +53,7 @@ if (!empty($arResult["SECTIONS"])) {
             $section["NAME"] = $section["UF_TITLE_MAIN"];
         }
 
+        $section["NAME"] = preg_replace("/&lt;br&gt;|&lt;br\s+\/&gt;/i", "", $section["NAME"]);
         $cntSectionsUpPosition = count($arSectionUpPosition);
 
         if ($arUfFiPosData[$section["UF_FI_POS"]] === "down") {
