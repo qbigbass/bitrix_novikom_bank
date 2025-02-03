2eccd15a3a2a18ea9572d6772f1091eb46fb5701
Mark Kim <mark.kim@dalee.ru>
2025-02-03 21:27:49 +0300

    master | pb fixes

--- local/templates/private_banking/header.php
+++ local/templates/private_banking/header.php
@@ -36,6 +36,10 @@ $asset->addJs('/frontend/dist/js/sliders.js');
 $asset->addJs('/frontend/dist/js/tabs.js');
 $asset->addJs('/frontend/dist/js/yMap.js');
 $asset->addJs('/frontend/dist/js/private-banking.js');
+$asset->addJs('/frontend/dist/js/calculator-deposit.js');
+$asset->addJs('/frontend/dist/js/calculator-loan.js');
+$asset->addJs('/frontend/dist/js/calculator-mortgage.js');
+$asset->addJs('/frontend/dist/js/calculator-bonus.js');
 $asset->addJs('/frontend/dist/js/charts.js');
 $asset->addJs('/frontend/dist/js/index.js');
 
