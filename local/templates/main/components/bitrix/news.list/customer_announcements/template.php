5e0f2f2911c0d3b335241610313dbf544232f596
Radik.M <r.machmutov@astarus.com>
2025-02-05 16:03:07 +0400

    DNVKBSITE-156 | Поправил шаблон для блока "Объявления для клиентов"

--- local/templates/main/components/bitrix/news.list/customer_announcements/template.php
+++ local/templates/main/components/bitrix/news.list/customer_announcements/template.php
@@ -18,51 +18,57 @@ $parentSection = $component->getParent()?->arParams['SECTION_URL'] ?? ($componen
         <div class="container">
             <div class="row">
                 <div class="col-12 col-xxl-6 d-flex flex-column gap-6 gap-lg-7">
-            <a class="h3 d-flex align-items-center ps-lg-6" href="/support/announcements_for_clients<?= $parentSection ?>">
-                <span><?= $arResult["NAME"] ?></span>
-                <span class="icon size-m violet-100 ms-auto ms-md-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-                        <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-right"></use>
-                    </svg>
-                </span>
-            </a>
-            <div class="ps-lg-6 mt-xxl-auto mw-100">
-                <div class="swiper js-announcement-slider">
-                    <div class="swiper-wrapper">
-                        <? foreach ($arResult["ITEMS"] as $item) { ?>
-                            <?
-                            $this->AddEditAction($item['ID'], $item['EDIT_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_EDIT"));
-                            $this->AddDeleteAction($item['ID'], $item['DELETE_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-                            ?>
-                            <div class="swiper-slide js-announcement-slide" id="<?=$this->GetEditAreaId($item['ID']);?>">
-                                <a class="announcement" href="<?= $item["DETAIL_PAGE_URL"] ?>" tabIndex="-1">
-                                    <span class="dark-70"><?= date('d.m.Y', strtotime($item["TIMESTAMP_X"])) ?></span>
-                                    <span class="dark-100"><?= $item["NAME"] ?></span>
-                                    <span class="icon size-m d-none d-md-inline-block ms-auto">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-                                            <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-arrow-right"></use>
-                                        </svg>
-                                    </span>
-                                </a>
-                            </div>
-                        <? } ?>
-                    </div>
-                    <div class="slider-controls js-swiper-controls mt-3 mt-md-4">
-                        <div class="slider-controls__pagination js-swiper-pagination"></div>
-                        <div class="slider-controls__navigation js-swiper-nav">
-                            <button class="swiper-button-prev js-swiper-prev" type="button"><span class="icon size-m">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-                                <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-left"></use>
-                              </svg></span></button>
-                            <button class="swiper-button-next js-swiper-next" type="button"><span class="icon size-m">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                    <a class="h3 d-flex align-items-center ps-lg-6" href="/support/announcements_for_clients<?= $parentSection ?>">
+                        <span><?= $arResult["NAME"] ?></span>
+                        <span class="icon size-m violet-100 ms-auto ms-md-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
                                 <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-right"></use>
-                              </svg></span></button>
+                            </svg>
+                        </span>
+                    </a>
+                    <div class="ps-lg-6 mt-xxl-auto mw-100">
+                        <div class="swiper js-announcement-slider">
+                            <div class="swiper-wrapper">
+                                <? foreach ($arResult["ITEMS"] as $item) { ?>
+                                    <?
+                                    $this->AddEditAction($item['ID'], $item['EDIT_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_EDIT"));
+                                    $this->AddDeleteAction($item['ID'], $item['DELETE_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                                    ?>
+                                    <div class="swiper-slide js-announcement-slide" id="<?=$this->GetEditAreaId($item['ID']);?>">
+                                        <a class="announcement" href="<?= $item["DETAIL_PAGE_URL"] ?>" tabIndex="-1">
+                                            <span class="dark-70"><?= date('d.m.Y', strtotime($item["TIMESTAMP_X"])) ?></span>
+                                            <span class="dark-100"><?= $item["NAME"] ?></span>
+                                            <span class="icon size-m d-none d-md-inline-block ms-auto">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                                    <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-arrow-right"></use>
+                                                </svg>
+                                            </span>
+                                        </a>
+                                    </div>
+                                <? } ?>
+                            </div>
+                            <div class="slider-controls js-swiper-controls mt-3 mt-md-4">
+                                <div class="slider-controls__pagination js-swiper-pagination"></div>
+                                <div class="slider-controls__navigation js-swiper-nav">
+                                    <button class="swiper-button-prev js-swiper-prev" type="button">
+                                        <span class="icon size-m">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                                <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-left"></use>
+                                            </svg>
+                                        </span>
+                                    </button>
+                                    <button class="swiper-button-next js-swiper-next" type="button">
+                                        <span class="icon size-m">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                                <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-chevron-right"></use>
+                                            </svg>
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
             </div>
         </div>
         <picture class="pattern-bg pattern-bg--hide-mobile">
