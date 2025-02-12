3ddb926124502835f5cd1d96be506df43af44a1b
Serghey Smetanin <sergheysmetanin@gmail.com>
2025-02-12 15:30:16 +0300

    DNVKBSITE-172 | Добавил два шаблона news.detail

---
+++ local/templates/main/components/bitrix/news/services/bitrix/news.detail/prochie_servisy/template.php
@@ -0,0 +1,117 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+
+use Dalee\Helpers\ComponentHelper;
+
+$parentTemplateFolder = $component->GetParent()->getTemplate()->GetFolder();
+$helper = new ComponentHelper($component);
+?>
+
+<?$APPLICATION->IncludeFile(
+    $parentTemplateFolder . '/include/header.php',
+    [
+        'helper' => $helper,
+        'arResult' => $arResult
+    ]
+)?>
+
+<?if(!empty($arResult['DISPLAY_PROPERTIES']['TABS']['VALUE'])) : ?>
+    <section class="section-layout">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?$GLOBALS['tabsFilter'] = [
+                        'ACTIVE' => 'Y',
+                        'ID' => $arResult['DISPLAY_PROPERTIES']['TABS']['VALUE']
+                    ];
+
+                    $APPLICATION->IncludeComponent(
+                        "bitrix:news.list",
+                        "tabs_other_services",
+                        [
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "CACHE_FILTER" => "N",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "FIELD_CODE" => ["CODE","NAME","PREVIEW_TEXT","PREVIEW_PICTURE",""],
+                            "FILTER_NAME" => "tabsFilter",
+                            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                            "IBLOCK_ID" => iblock('tabs'),
+                            "IBLOCK_TYPE" => "additional",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "INCLUDE_SUBSECTIONS" => "N",
+                            "MESSAGE_404" => "",
+                            "NEWS_COUNT" => "20",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_DESC_NUMBERING" => "N",
+                            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_SHOW_ALWAYS" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Новости",
+                            "PARENT_SECTION" => "",
+                            "PARENT_SECTION_CODE" => "",
+                            "PREVIEW_TRUNCATE_LEN" => "",
+                            "PROPERTY_CODE" => [
+                                "CONDITIONS_ICONS",
+                                "CONDITIONS",
+                                "RATES_DESCRIPTION",
+                                "TEXT_FIELD",
+                                "HEADING",
+                                "STEPS",
+                                "SHORT_INFO",
+                                "QUOTES",
+                                "QUESTIONS",
+                                "ICONS_WITH_DESCRIPTION",
+                                "ICON_SHORT_INFO",
+                                "DOCUMENTS",
+                                "TARIFFS",
+                                "BENEFITS"
+                            ],
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "SORT_BY1" => "ACTIVE_FROM",
+                            "SORT_BY2" => "SORT",
+                            "SORT_ORDER1" => "DESC",
+                            "SORT_ORDER2" => "ASC",
+                            "STRICT_SECTION_CHECK" => "N",
+                        ],
+                        $component
+                    );?>
+                </div>
+            </div>
+        </div>
+        <picture class="pattern-bg pattern-bg--hide-mobile">
+            <source srcset="/frontend/dist/img/patterns/section-2/pattern-light-s.svg" media="(max-width: 767px)">
+            <source srcset="/frontend/dist/img/patterns/section-2/pattern-light-m.svg" media="(max-width: 1199px)">
+            <img src="/frontend/dist/img/patterns/section-2/pattern-light-l.svg" alt="bg pattern" loading="lazy">
+        </picture>
+    </section>
+<?endif;?>
+<?$helper->saveCache();?>
