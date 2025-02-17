9b4932777863e2b0d2a6cae6b17767e24252aa08
Radik.M <r.machmutov@astarus.com>
2025-02-17 15:24:11 +0400

    DNVKBSITE-176 | Удалил неиспользуемые шаблоны компонентов для старой структуры МСБ и ФИ

--- local/templates/main/components/bitrix/catalog.section.list/fi_catalog/lang/ru/template.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MESS["FL_CATALOG_LINK_DETAIL_TITLE"] = "Подробнее";
-$MESS["FL_CATALOG_LINK_DETAIL_TITLE_DOWN"] = "Узнать больше";
-$MESS["FL_CATALOG_SHOW_ALL_TITLE"] = "Показать все";
--- local/templates/main/components/bitrix/news.list/fi_ads_clients/lang/ru/template.php
+++
@@ -1,2 +0,0 @@
-<?
-$MESS["ADS_BLOCK_TITLE"] = "Объявления <span class='d-none d-md-inline'>для клиентов</span>";
