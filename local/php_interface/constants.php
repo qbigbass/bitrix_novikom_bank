8c74d2abd7401c7168bd88f230a53048e881101a
Serghey Smetanin <sergheysmetanin@gmail.com>
2025-02-18 17:34:50 +0300

    DNVKBSITE-181 | Уменьшение размера бэкграунд картинок

--- local/php_interface/constants.php
+++ local/php_interface/constants.php
@@ -2,8 +2,6 @@
 const MOBIL_APP_LINK = '/upload/android-v19.apk';
 const ONLINE_BANK_LINK = '#';
 const OFFICES_AND_ATMS_LINK = '/map/offices/';
-const MOBIL_PHONE_CONTACT_NUMBER = '+7 (800) 250-70-07';
-const MOBIL_PHONE_CONTACT_NUMBER_2 = '+7 (495) 974-71-87';
 const RU_STORE_APP_LINK = 'https://apps.rustore.ru/app/com.bssys.novikomretail';
 const RU_MARKET_APP_LINK = 'https://ruplay.market/apps/com.bssys.novikomretail';
 const NASH_STORE_APP_LINK = 'https://store.nashstore.ru/store/65b74fde0a39b29c04ad1f2f';
@@ -26,8 +24,8 @@ const UF_CHATBOT_MESSAGE = 'Просто текст для чатбота';
  */
 const UF_CHATBOT_FILE = '/upload/uf/2aa/y52ouwppvq3k4s17i8c8w3r6mgho0rhe/icons8-%D0%B1%D0%BE%D1%82-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-50.png';
 /** @var string Телефон 1 */
-const UF_PHONE1 = MOBIL_PHONE_CONTACT_NUMBER;
+const UF_PHONE1 = '+7 (800) 250-70-07';
 /** @var string Телефон 2 */
-const UF_PHONE2 = MOBIL_PHONE_CONTACT_NUMBER_2;
+const UF_PHONE2 = '+7 (495) 974-71-87';
 /** @var string Длительность отображения слайда на главной слайдере */
 const UF_BANNER_DELAY = 5000;
