f5f3b5355a82c8ee2e735cb2ea9e9fd9a57916c0
Serghey Smetanin <sergheysmetanin@gmail.com>
2025-02-27 12:08:35 +0300

    DNVKBSITE-141 | Private banking убрал JSON из свойств

---
+++ local/templates/private_banking/lang/ru/header.php
@@ -0,0 +1,4 @@
+<?php
+$MESS['HEADER_MENU_BECOME_A_CLIENT'] = 'Стать клиентом';
+$MESS['HEADER_MAIN_SITE'] = 'Основной сайт Новиком';
+$MESS['HEADER_MAIN_TITLE'] = 'Лучшие финансовые инструменты <br>для&nbsp;достижения ваших целей';
