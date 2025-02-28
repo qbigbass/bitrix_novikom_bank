88c4930bf1d4d47e15436d3503b7f97739ec1c62
Serghey Smetanin <sergheysmetanin@gmail.com>
2025-02-28 11:09:47 +0300

    DNVKBSITE-141 | Контакты на private banking через ИБ

---
+++ local/templates/private_banking/components/bitrix/news.detail/contancts/template.php
@@ -0,0 +1,185 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use Bitrix\Main\Localization\Loc;
+
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$phone1 = $arResult['PROPERTIES']['PHONE']['VALUE'][0];
+$phone2 = $arResult['PROPERTIES']['PHONE']['VALUE'][1];
+$address1 = $arResult['PROPERTIES']['ADDRESS']['VALUE'][0];
+$address2 = $arResult['PROPERTIES']['ADDRESS']['VALUE'][1];
+$email = $arResult['PROPERTIES']['EMAIL']['VALUE'];
+$copyright = $arResult['~PREVIEW_TEXT'];
+$qr = $arResult['PREVIEW_PICTURE']['SRC'];
+$map = $arResult['DETAIL_PICTURE']['SRC'];
+
+?>
+<div class="mt-auto pt-7 pt-lg-0">
+    <div class="pb-card-contact d-flex flex-column row-gap-4 row-gap-lg-5 align-items-lg-center">
+        <ul class="list-pb-contact d-flex flex-column flex-lg-row justify-content-xl-between flex-wrap gap-4">
+            <?
+            if ($phone1) {
+                ?>
+                <li class="d-flex align-items-center">
+                                <span class="icon size-m flex-shrink-0 dark-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                        <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-phone"></use>
+                                    </svg>
+                                </span>
+                    <a class="list-pb-contact__link"
+                       href="tel:+<?= preg_replace('/\D+/', '', $phone1); ?>"
+                       data-phone="<?= $phone1 ?>"><?= $phone1 ?></a>
+                </li>
+                <?
+            }
+            ?>
+            <?
+            if ($email) {
+                ?>
+                <li class="d-flex align-items-center">
+                                <span class="icon size-m flex-shrink-0 dark-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                        <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-mail"></use>
+                                    </svg>
+                                </span>
+                    <a class="list-pb-contact__link"
+                       href="emailto:<?= $email ?>"><?= $email ?></a>
+                </li>
+                <?
+            }
+            ?>
+            <?
+            if ($address2) {
+                ?>
+                <li class="d-flex align-items-center">
+                                <span class="icon size-m flex-shrink-0 dark-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                                        <use xlink:href="/frontend/dist/img/svg-sprite.svg#icon-bank"></use>
+                                    </svg>
+                                </span>
+                    <span><?= $address2 ?></span>
+                </li>
+                <?
+            }
+            ?>
+        </ul>
+        <a class="btn btn-pb btn-pb--outline" href="/"><?= Loc::getMessage('HEADER_MAIN_SITE') ?></a>
+    </div>
+</div>
+
+<?
+$this->SetViewTarget('PB_INDEX_CONTACTS'); ?>
+<section class="pb-section pb-section--bg-black">
+    <div class="container">
+        <div class="pb-card-office animate js-animation">
+            <div class="pb-card-office__content d-flex flex-column align-items-start">
+                <h3 class="pb-card-office__title mb-3 mb-lg-4">Контакты</h3>
+                <?
+                if ($address1) {
+                    ?>
+                    <p class="pb-card-office__text pr-text-color mb-0"><?= UF_PB_FULL_ADDRESS ?></p>
+                    <?
+                }
+                ?>
+                <div class="mt-auto d-flex justify-content-between align-items-center w-100">
+                    <ul class="pb-card-office__list d-flex flex-column row-gap-2 row-gap-md-3 align-items-start">
+                        <?
+                        if ($email) {
+                            ?>
+                            <li><a href="mailto:<?= $email ?>"><?= $email ?></a></li>
+                            <?
+                        }
+                        ?>
+                        <?
+                        if ($phone1) {
+                            ?>
+                            <li>
+                                <a href="tel:+<?= preg_replace('/\D+/', '', $phone1); ?>">
+                                    <?= $phone1 ?>
+                                </a>
+                            </li>
+                            <?
+                        }
+                        ?>
+                    </ul>
+                    <?
+                    if ($qr) {
+                        ?>
+                        <div class="pb-card-office__qr d-none d-xl-block">
+                            <img src="<?= $qr ?>" alt="qr-код" loading="lazy" width="80" height="80">
+                        </div>
+                        <?
+                    }
+                    ?>
+                </div>
+            </div>
+            <?
+            if ($map) {
+                ?>
+                <div class="pb-card-office__map">
+                    <img src="<?= $map ?>" alt="qr-код" loading="lazy" width="80" height="80" alt="карта"
+                         loading="lazy">
+                </div>
+                <?
+            }
+            ?>
+        </div>
+    </div>
+</section>
+<?
+$this->EndViewTarget(); ?>
+
+<?
+$this->SetViewTarget('PB_FOOTER_CONTACTS'); ?>
+<div class="container">
+    <div class="row row-gap-5">
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column row-gap-5 flex-lg-row gap-lg-6 align-items-lg-center"><img
+                    src="/frontend/dist/img/logo-pb-footer.svg" alt="Новиком" width="196" height="56" loading="lazy">
+                <?
+                if ($copyright) {
+                    ?>
+                    <p class="pb-footer__copyright m-0"><?= str_replace('#DATE#', date('Y'), $copyright) ?></p>
+                    <?
+                }
+                ?>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column row-gap-2 align-items-start align-items-lg-end">
+                <?
+                if ($phone1) {
+                    ?>
+                    <a class="pb-footer__text pb-footer__text--phone"
+                       href="tel:+<?= preg_replace('/\D+/', '', UF_PHONE1); ?>">
+                        <?= UF_PHONE1 ?>
+                    </a>
+                    <?
+                }
+                ?>
+                <?
+                if ($address1) {
+                    ?>
+                    <p class="m-0 pb-footer__text"><?= UF_PB_FULL_ADDRESS ?></p>
+                    <?
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?
+$this->EndViewTarget(); ?>
--- s1/urlrewrite.php
+++ s1/urlrewrite.php
@@ -1,227 +1,259 @@
 <?php
-$arUrlRewrite = [
-    [
-        'CONDITION' => '#^/for-corporate-clients/mery-gospodderzhki/[a-z-]+/[a-z-\d]+/#',
-        'RULE' => '',
-        'ID' => '',
-        'PATH' => '/for-corporate-clients/financing_measures/detail.php',
-        'SORT' => 1,
-    ],
-    [
-        'CONDITION' => '#^/for-corporate-clients/mery-gospodderzhki/[a-z-]+/#',
-        'RULE' => '',
-        'ID' => '',
-        'PATH' => '/for-corporate-clients/financing_measures/section.php',
-        'SORT' => 2,
-    ],
-    [
-        'CONDITION' => '#^/for-corporate-clients/customer-announcements/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/for-corporate-clients/customer-announcements/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/support/very_important_information/#',
-        'RULE' => '',
-        'ID' => 'bitrix:catalog',
-        'PATH' => '/support/very_important_information/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/support/announcements_for_clients/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/support/announcements_for_clients/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/support/questions_and_answers/([^/]+)(/.*|$)#',
-        'RULE' => 'path=$1',
-        'ID' => 'bitrix:news',
-        'PATH' => '/support/questions_and_answers/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/support/products_services/#',
-        'RULE' => '',
-        'ID' => 'bitrix:catalog',
-        'PATH' => '/support/products_services/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/payments-and-transfers/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/payments-and-transfers/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/financial-institutions/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/financial-institutions/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/for-corporate-clients/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/for-corporate-clients/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/loans/restructuring/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/loans/restructuring/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/about/press-center/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/about/press-center/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/about/purchases/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/about/purchases/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/bonus-programs/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/bonus-programs/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/special-offers/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/special-offers/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/about/career/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/about/career/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/mortgage/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/mortgage/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/deposits/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/deposits/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/services/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/services/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/cards/[a-z-]+/[a-z-]+/#',
-        'RULE' => '',
-        'ID' => '',
-        'PATH' => '/cards/detail.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/cards/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/cards/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/about/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/about/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/loans/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/loans/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/msb/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/msb/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/map/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/map/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/en/about/#',
-        'RULE' => '',
-        'ID' => '',
-        'PATH' => '/en/about/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/en/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/en/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/private-banking/services/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/private-banking/services/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/private-banking/cards/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/private-banking/cards/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/insurance/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/insurance/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/documents/(.+)#',
-        'RULE' => 'path=$1',
-        'ID' => '',
-        'PATH' => '/documents/index.php',
-        'SORT' => 100,
-    ],
-    [
-        'CONDITION' => '#^/online-services/[a-z-]+/#',
-        'RULE' => '',
-        'ID' => '',
-        'PATH' => '/online-services/index.php',
-        'SORT' => 100,
-    ],
-];
+$arUrlRewrite=array (
+  0 => 
+  array (
+    'CONDITION' => '#^/for-corporate-clients/mery-gospodderzhki/[a-z-]+/[a-z-\\d]+/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/for-corporate-clients/financing_measures/detail.php',
+    'SORT' => 1,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/for-corporate-clients/mery-gospodderzhki/[a-z-]+/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/for-corporate-clients/financing_measures/section.php',
+    'SORT' => 2,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/for-corporate-clients/customer-announcements/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/for-corporate-clients/customer-announcements/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/support/questions_and_answers/([^/]+)(/.*|$)#',
+    'RULE' => 'path=$1',
+    'ID' => 'bitrix:news',
+    'PATH' => '/support/questions_and_answers/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/support/very_important_information/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/support/very_important_information/index.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/support/announcements_for_clients/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/support/announcements_for_clients/index.php',
+    'SORT' => 100,
+  ),
+  6 => 
+  array (
+    'CONDITION' => '#^/support/products_services/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/support/products_services/index.php',
+    'SORT' => 100,
+  ),
+  27 => 
+  array (
+    'CONDITION' => '#^/private-banking/services/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/private-banking/services/index.php',
+    'SORT' => 100,
+  ),
+  31 => 
+  array (
+    'CONDITION' => '#^/online-services/[a-z-]+/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/online-services/index.php',
+    'SORT' => 100,
+  ),
+  7 => 
+  array (
+    'CONDITION' => '#^/payments-and-transfers/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/payments-and-transfers/index.php',
+    'SORT' => 100,
+  ),
+  8 => 
+  array (
+    'CONDITION' => '#^/financial-institutions/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/financial-institutions/index.php',
+    'SORT' => 100,
+  ),
+  9 => 
+  array (
+    'CONDITION' => '#^/for-corporate-clients/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/for-corporate-clients/index.php',
+    'SORT' => 100,
+  ),
+  19 => 
+  array (
+    'CONDITION' => '#^/cards/[a-z-]+/[a-z-]+/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/cards/detail.php',
+    'SORT' => 100,
+  ),
+  28 => 
+  array (
+    'CONDITION' => '#^/private-banking/cards/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/private-banking/cards/index.php',
+    'SORT' => 100,
+  ),
+  10 => 
+  array (
+    'CONDITION' => '#^/loans/restructuring/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/loans/restructuring/index.php',
+    'SORT' => 100,
+  ),
+  11 => 
+  array (
+    'CONDITION' => '#^/about/press-center/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/about/press-center/index.php',
+    'SORT' => 100,
+  ),
+  12 => 
+  array (
+    'CONDITION' => '#^/about/purchases/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/about/purchases/index.php',
+    'SORT' => 100,
+  ),
+  13 => 
+  array (
+    'CONDITION' => '#^/bonus-programs/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/bonus-programs/index.php',
+    'SORT' => 100,
+  ),
+  14 => 
+  array (
+    'CONDITION' => '#^/special-offers/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/special-offers/index.php',
+    'SORT' => 100,
+  ),
+  30 => 
+  array (
+    'CONDITION' => '#^/documents/(.+)#',
+    'RULE' => 'path=$1',
+    'ID' => '',
+    'PATH' => '/documents/index.php',
+    'SORT' => 100,
+  ),
+  15 => 
+  array (
+    'CONDITION' => '#^/about/career/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/about/career/index.php',
+    'SORT' => 100,
+  ),
+  29 => 
+  array (
+    'CONDITION' => '#^/insurance/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/insurance/index.php',
+    'SORT' => 100,
+  ),
+  16 => 
+  array (
+    'CONDITION' => '#^/mortgage/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/mortgage/index.php',
+    'SORT' => 100,
+  ),
+  17 => 
+  array (
+    'CONDITION' => '#^/deposits/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/deposits/index.php',
+    'SORT' => 100,
+  ),
+  18 => 
+  array (
+    'CONDITION' => '#^/services/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/services/index.php',
+    'SORT' => 100,
+  ),
+  25 => 
+  array (
+    'CONDITION' => '#^/en/about/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/en/about/index.php',
+    'SORT' => 100,
+  ),
+  20 => 
+  array (
+    'CONDITION' => '#^/cards/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/cards/index.php',
+    'SORT' => 100,
+  ),
+  21 => 
+  array (
+    'CONDITION' => '#^/about/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/about/index.php',
+    'SORT' => 100,
+  ),
+  22 => 
+  array (
+    'CONDITION' => '#^/loans/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/loans/index.php',
+    'SORT' => 100,
+  ),
+  23 => 
+  array (
+    'CONDITION' => '#^/msb/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/msb/index.php',
+    'SORT' => 100,
+  ),
+  24 => 
+  array (
+    'CONDITION' => '#^/map/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/map/index.php',
+    'SORT' => 100,
+  ),
+  26 => 
+  array (
+    'CONDITION' => '#^/en/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/en/index.php',
+    'SORT' => 100,
+  ),
+);
