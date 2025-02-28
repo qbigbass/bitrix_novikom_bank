3c9b25fef4f8eb96eccd51315cc8c1f24f35edf2
Serghey Smetanin <sergheysmetanin@gmail.com>
2025-02-28 12:54:35 +0300

    DNVKBSITE-206 | Вывел 3-й уровень в карту сайта

--- s1/sitemap/index.php
+++ s1/sitemap/index.php
@@ -6,15 +6,15 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
 $APPLICATION->SetTitle("Карта сайта");
 
-$APPLICATION->IncludeComponent(
-    "bitrix:main.map",
-    ".default", [
-        "LEVEL" => "3",
-        "COL_NUM" => "2",
-        "SHOW_DESCRIPTION" => "Y",
-        "SET_TITLE" => "Y",
-        "CACHE_TIME" => "36000000"
-    ]
+$APPLICATION->IncludeComponent("dalee:main.map", ".default", Array(
+	    "LEVEL" => "3",	// Максимальный уровень вложенности (0 - без вложенности)
+		"COL_NUM" => "2",	// Количество колонок
+		"SHOW_DESCRIPTION" => "Y",	// Показывать описания
+        'THIRD_LEVEL_MENU_TYPE' => 'iblock_sections', // Тип меню для 3-е уровня
+		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+	),
+	false
 );
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
