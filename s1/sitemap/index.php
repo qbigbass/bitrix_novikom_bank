f9884e7c9a9ad673dc06a3ef9de8c5e6136a1772
Serghey Smetanin <sergheysmetanin@gmail.com>
2025-02-28 15:39:20 +0300

    DNVKBSITE-206 | review

--- s1/sitemap/index.php
+++ s1/sitemap/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @global CMain $APPLICATION
  */
@@ -6,15 +7,18 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
 $APPLICATION->SetTitle("Карта сайта");
 
-$APPLICATION->IncludeComponent("dalee:main.map", ".default", Array(
-	    "LEVEL" => "3",	// Максимальный уровень вложенности (0 - без вложенности)
-		"COL_NUM" => "2",	// Количество колонок
-		"SHOW_DESCRIPTION" => "Y",	// Показывать описания
+$APPLICATION->IncludeComponent(
+    "dalee:main.map",
+    ".default",
+    array(
+        "LEVEL" => "3",    // Максимальный уровень вложенности (0 - без вложенности)
+        "COL_NUM" => "2",    // Количество колонок
+        "SHOW_DESCRIPTION" => "Y",    // Показывать описания
         'THIRD_LEVEL_MENU_TYPE' => 'iblock_sections', // Тип меню для 3-е уровня
-		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-	),
-	false
+        "SET_TITLE" => "Y",    // Устанавливать заголовок страницы
+        "CACHE_TIME" => "36000000",    // Время кеширования (сек.)
+    ),
+    false
 );
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
