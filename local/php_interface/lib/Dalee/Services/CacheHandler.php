1e0dbbfc22b4f84d0d75730bb27ee75b545ceaf0
vimbatu <vimbatu@yandex.ru>
2025-03-04 11:51:29 +0300

    GLGNVKB-12 | правка сброс кеша связанных инфоблоков

--- local/php_interface/lib/Dalee/Services/CacheHandler.php
+++ local/php_interface/lib/Dalee/Services/CacheHandler.php
@@ -2,34 +2,31 @@
 
 namespace Dalee\Services;
 
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
+
 class CacheHandler
 {
     /**
      * @param $arFields
      * @return void
+     * @throws ArgumentException
+     * @throws ObjectPropertyException
+     * @throws SystemException
      */
     public static function onAfterIBlockElementUpdateHandler($arFields): void
     {
         global $DB, $CACHE_MANAGER;
 
-        $sourceIBlockIds = [
-            iblock('banking_support_options'),
-            iblock('tabs')
-        ];
+        $res = $DB->Query(
+            "SELECT IBLOCK_ID FROM b_iblock_property WHERE LINK_IBLOCK_ID = " . $DB->ForSql(
+                $arFields['IBLOCK_ID']
+            )
+        );
 
-        foreach ($sourceIBlockIds as $iblockId) {
-            if ($arFields["IBLOCK_ID"] == $iblockId) {
-
-                $res = $DB->Query(
-                    "SELECT IBLOCK_ID FROM b_iblock_property WHERE LINK_IBLOCK_ID = " . $DB->ForSql(
-                        $arFields['IBLOCK_ID']
-                    )
-                );
-
-                while ($row = $res->Fetch()) {
-                    $CACHE_MANAGER->ClearByTag('iblock_id_' . $row['IBLOCK_ID']);
-                }
-            }
+        while ($row = $res->Fetch()) {
+            $CACHE_MANAGER->ClearByTag('iblock_id_' . $row['IBLOCK_ID']);
         }
     }
 }
