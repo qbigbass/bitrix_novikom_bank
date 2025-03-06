522654be69d6947b82339446bfb45dacb938b823
Sergey Smetanin <sergheysmetnain@gmail.com>
2025-03-06 12:31:56 +0300

    DNVKBSITE-141 | Баглист 22 пункт

--- s1/private-banking/.top.menu.php
+++ s1/private-banking/.top.menu.php
@@ -1,32 +1,25 @@
 <?
 $aMenuLinks = Array(
 	Array(
-		"Главная", 
-		"/private-banking/", 
-		Array(), 
-		Array(), 
-		"" 
+		"Главная",
+		"/private-banking/",
+		Array(),
+		Array(),
+		""
 	),
 	Array(
-		"Инвестиционные услуги", 
-		"/private-banking/services/investment/brokerskoe-obsluzhivanie/", 
-		Array(), 
-		Array(), 
-		"" 
+		"Инвестиционные услуги",
+		"/private-banking/services/investment/brokerskoe-obsluzhivanie/",
+		Array(),
+		Array(),
+		""
 	),
 	Array(
-		"Карта Мир Supreme", 
-		"/private-banking/cards/mir-supreme-card/", 
-		Array(), 
-		Array(), 
-		"" 
+		"Карта Мир Supreme",
+		"/private-banking/cards/mir-supreme-card/",
+		Array(),
+		Array(),
+		""
 	),
-	Array(
-		"Онлайн-банк", 
-		"https://online.novikom.ru/#/registration", 
-		Array(), 
-		Array("CLASS"=>"btn btn-pb btn-pb--outline d-none d-lg-inline-block"), 
-		"" 
-	)
 );
 ?>
