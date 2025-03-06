8b1df66fbcee72cfa06244d878f95ac2be5fd229
Mark Kim <mkim@dalee.ru>
2025-03-06 14:38:07 +0300

    DNVKBSITE-141 | code style

--- s1/private-banking/.top.menu.php
+++ s1/private-banking/.top.menu.php
@@ -1,19 +1,19 @@
-<?
-$aMenuLinks = Array(
-	Array(
-		"Главная",
-		"/private-banking/",
-		Array(),
-		Array(),
-		""
-	),
-	Array(
-		"Инвестиционные услуги",
-		"/private-banking/services/investment/brokerskoe-obsluzhivanie/",
-		Array(),
-		Array(),
-		""
-	),
+<?php
+$aMenuLinks = array(
+    array(
+        "Главная",
+        "/private-banking/",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Инвестиционные услуги",
+        "/private-banking/services/investment/brokerskoe-obsluzhivanie/",
+        array(),
+        array(),
+        ""
+    ),
     array(
         "Карта Мир Supreme",
         "/private-banking/cards/mir-supreme-card/",
@@ -22,4 +22,3 @@ $aMenuLinks = Array(
         ""
     ),
 );
-?>
