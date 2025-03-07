ed766072d8f67860f990fd134d086f3bc2f71ee1
Radik.M <r.machmutov@astarus.com>
2025-03-07 15:29:59 +0400

    DNVKBSITE-176 | Добавил свойства к разделам для цветовой палитры во включаемом блоке "Контакты".

--- s1/financial-institutions/.section.php
+++ s1/financial-institutions/.section.php
@@ -3,7 +3,12 @@ $sSectionName = "Финансовым институтам";
 $arDirProperties = [
     "description" => "",
     "templateSubMenu" => "corporate_submenu_header",
-    "hiddenKey" => 4
+    "hiddenKey" => 4,
+    "defaultColorCard" => "contact-block--bg-gray",
+    "defaultColorTag" => "tag--outline",
+    "defaultColorH4" => "dark-100",
+    "defaultColorSpan" => "dark-100",
+    "defaultColorIcon" => "violet-100"
 ];
 ?>
 
