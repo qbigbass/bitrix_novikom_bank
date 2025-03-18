0e9465f3637c49b4adfe99cd9a74833d8d40a60e
v.skokov <vladislav.skokov@dalee.ru>
2025-03-18 09:53:01 +0300

    DNKVBSITE-223 Переработка страницы контактов раздела о банке

---
+++ local/php_interface/migrations/Version20250317195640.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20250317195640 extends Version
+{
+    protected $author = "vladislav.skokov@dalee.ru";
+
+    protected $description = "Миграция ИБ О банке - Контакты";
+
+    protected $moduleVersion = "4.18.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $iblockId = $helper->Iblock()->saveIblock(array (
+  'IBLOCK_TYPE_ID' => 'about_ru',
+  'LID' => 
+  array (
+    0 => 's1',
+  ),
+  'CODE' => 'contact_info_ru',
+  'API_CODE' => 'contactInfoRu',
+  'REST_ON' => 'N',
+  'NAME' => 'Контакты банка Новиком',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'LIST_PAGE_URL' => '',
+  'DETAIL_PAGE_URL' => '',
+  'SECTION_PAGE_URL' => '',
+  'CANONICAL_PAGE_URL' => '',
+  'PICTURE' => NULL,
+  'DESCRIPTION' => '',
+  'DESCRIPTION_TYPE' => 'text',
+  'RSS_TTL' => '24',
+  'RSS_ACTIVE' => 'Y',
+  'RSS_FILE_ACTIVE' => 'N',
+  'RSS_FILE_LIMIT' => NULL,
+  'RSS_FILE_DAYS' => NULL,
+  'RSS_YANDEX_ACTIVE' => 'N',
+  'XML_ID' => NULL,
+  'INDEX_ELEMENT' => 'Y',
+  'INDEX_SECTION' => 'Y',
+  'WORKFLOW' => 'N',
+  'BIZPROC' => 'N',
+  'SECTION_CHOOSER' => 'L',
+  'LIST_MODE' => '',
+  'RIGHTS_MODE' => 'S',
+  'SECTION_PROPERTY' => 'N',
+  'PROPERTY_INDEX' => 'N',
+  'VERSION' => '1',
+  'LAST_CONV_ELEMENT' => '0',
+  'SOCNET_GROUP_ID' => NULL,
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'SECTIONS_NAME' => 'Разделы',
+  'SECTION_NAME' => 'Раздел',
+  'ELEMENTS_NAME' => 'Элементы',
+  'ELEMENT_NAME' => 'Элемент',
+  'EXTERNAL_ID' => NULL,
+  'LANG_DIR' => '/',
+  'IPROPERTY_TEMPLATES' => 
+  array (
+  ),
+  'ELEMENT_ADD' => 'Добавить элемент',
+  'ELEMENT_EDIT' => 'Изменить элемент',
+  'ELEMENT_DELETE' => 'Удалить элемент',
+  'SECTION_ADD' => 'Добавить раздел',
+  'SECTION_EDIT' => 'Изменить раздел',
+  'SECTION_DELETE' => 'Удалить раздел',
+));
+    $helper->Iblock()->saveGroupPermissions($iblockId, array (
+  'administrators' => 'X',
+));
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Бэкграунд',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'BANNER_BACKGROUND',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'F',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '1',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->UserOptions()->saveElementForm($iblockId, array (
+  'Параметры|edit1' => 
+  array (
+    'ID' => 'ID',
+    'DATE_CREATE' => 'Создан',
+    'TIMESTAMP_X' => 'Изменен',
+    'ACTIVE' => 'Активность',
+    'ACTIVE_FROM' => 'Начало активности',
+    'ACTIVE_TO' => 'Окончание активности',
+    'NAME' => 'Название',
+    'CODE' => 'Символьный код',
+    'SORT' => 'Сортировка',
+    'PREVIEW_TEXT' => 'Описание',
+  ),
+  'Детальная страница|cedit1' => 
+  array (
+    'DETAIL_PICTURE' => 'Детальная картинка',
+    'PROPERTY_BANNER_BACKGROUND' => 'Бэкграунд баннера',
+    'DETAIL_TEXT' => 'Детальный текст',
+  ),
+));
+    $helper->UserOptions()->saveElementGrid($iblockId, array (
+  'views' => 
+  array (
+    'default' => 
+    array (
+      'columns' => 
+      array (
+        0 => '',
+      ),
+      'columns_sizes' => 
+      array (
+        'expand' => 1,
+        'columns' => 
+        array (
+        ),
+      ),
+      'sticked_columns' => 
+      array (
+      ),
+      'custom_names' => 
+      array (
+      ),
+    ),
+  ),
+  'filters' => 
+  array (
+  ),
+  'current_view' => 'default',
+));
+
+    }
+}
