589caab0556aea2b5be2bf50d2d0a10fd8a35ebe
vimbatu <vimbatu@yandex.ru>
2025-03-24 13:54:53 +0300

    доработка по дейтпикеру

--- local/templates/main/components/bitrix/catalog.smart.filter/ads_filter/script.js
+++ local/templates/main/components/bitrix/catalog.smart.filter/ads_filter/script.js
@@ -1,3 +1,12 @@
 function setFilter(select) {
     window.location.href = select.value;
 }
+
+$(document).ready(function () {
+    $('.js-date').on('select', function () {
+        const selectedDate = $(this).val();
+        if (selectedDate.includes(' - ')) {
+            $(this).closest('form').submit();
+        }
+    });
+});
